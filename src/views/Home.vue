c117a91c5810155679f04cee0e38998f09c68dee
meng <2896279075@qq.com>
2023-03-09 12:01:33 +0800

    Topnav组件封装完成

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Topnav />
+  </div>
+  <div class="banner">
+    <h1>轱辘UI</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: { Topnav },
+};
+</script>
+
+<style lang="scss" scoped></style>
