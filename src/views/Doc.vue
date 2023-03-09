f9564a09a4a2504b401a883da4f4b9aa00a645e2
meng <2896279075@qq.com>
2023-03-09 13:53:54 +0800

    调节topnav 和 aside

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,10 +1,16 @@
-import Topnav from '../components/Topnav.vue';
-
 <template>
   <div>
     <Topnav />
     <div class="content">
-      <aside>边栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li><router-link to="/doc/switch">Switch组件</router-link></li>
+          <li><router-link to="/doc/button">Button组件</router-link></li>
+          <li><router-link to="/doc/dialog">Dialog组件</router-link></li>
+          <li><router-link to="/doc/tab">Tabs组件</router-link></li>
+        </ol>
+      </aside>
       <main>主内容</main>
     </div>
   </div>
@@ -18,3 +24,23 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150;
+  padding: 16px;
+  padding-top: 70px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
