1fd2a133373d0a612cedb01237d88461cc4afc9a
meng <2896279075@qq.com>
2023-03-30 10:19:58 +0800

    Dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,33 @@
 <template>
   <div>Dialog示例页面</div>
+  <h1>示例1</h1>
+  <ButtonV @click="toggle">toggle</ButtonV>
+  <Dialog
+    v-model:visible="x"
+    :onClickOverlay="true"
+    :ok="f1"
+    :cancel="f2"></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+import ButtonV from "../lib/ButtonV.vue";
+import { ref } from "vue";
+export default {
+  components: {
+    Dialog,
+    ButtonV,
+  },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      return true;
+    };
+    const f2 = () => {};
+    return { x, toggle, f1, f2 };
+  },
+};
+</script>
